4c598f9013e810d16c27b096cfcd1f62652e75a1
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-27 11:42:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,18 @@
 class Dog
-  def initialize(name, bark = "woof")
+  def name=(name)
     @name = name
-    @bark = bark
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark
+    puts = "woof"
   end
 
 end
+
+fido = Dog.new
+fido.name = ("Fido")
